2896f20697868b96ccd407ff480f26c3a8906492
Екатерина <79111771378@yandex.ru>
2020-08-23 22:04:04 +0300

    onclick widget hide-show

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ $(function(){
 		return false;
 	});
 });
+
+
+function hide_widget() {
+	document.getElementById("hide-widget").setAttribute("style", "display: none");
+	document.getElementById("show-widget").setAttribute("style", "display: block");
+	document.getElementById("widget-wa").setAttribute("style", "width: 70px");
+	document.getElementById("widget-text").setAttribute("style", "display: none");
+
+
+}
+
+function show_widget() {
+	document.getElementById("show-widget").setAttribute("style", "display: none");
+	document.getElementById("hide-widget").setAttribute("style", "display: block");
+	document.getElementById("widget-wa").setAttribute("style", "width: 200px");
+	document.getElementById("widget-text").setAttribute("style", "display: flex");
+
+}
